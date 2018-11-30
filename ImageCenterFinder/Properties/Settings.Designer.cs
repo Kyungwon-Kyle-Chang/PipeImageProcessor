a7367ab9423390ebcbb1825f42c61fede5d2726a
Kyungwon Chang <kwmnusa@naver.com>
2018-11-30 14:33:53 +0900

    Changed Project Name

--- ImageCenterFinder/Properties/Settings.Designer.cs
+++ ImageCenterFinder/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ImageCenterFinder.Properties {
+namespace PipeImageProcessor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
